93a2ac7dabe520fe2b9923e7f4be3a9b9c922b00
ANormalDude <wasim.khan202000.ib@gmail.com>
2022-03-30 04:19:01 +0100

    Login Button added

--- react/react-router-v6-example/src/App.js
+++ react/react-router-v6-example/src/App.js
@@ -6,26 +6,66 @@ import {
   Route,
   Link,
   Outlet,
-  useParams
+  useParams,
+  NavLink,
+  Navigate,
+  useNavigate,
+  useLocation,
 } from 'react-router-dom';
+const fakeAuth = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve('2342f2f1d131rf12'), 250);
+  });
 
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-}
+const AuthContext = React.createContext(null);
 
-function About() {
+const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [token, setToken] = React.useState(null);
+
+  const handleLogin = async () => {
+    const token = await fakeAuth();
+
+    setToken(token);
+
+    const origin = location.state?.from?.pathname || '/dashboard';
+    navigate(origin);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
+  const value = {
+    token,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
   return (
-    <div style={{ padding: 20 }}>
-      <h2>About View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
   );
-}
+};
+
+const useAuth = () => {
+  return React.useContext(AuthContext);
+};
+
+const ProtectedRoute = ({ children }) => {
+  const { token } = useAuth();
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  return children;
+};
+
 
 const BlogPosts = {
   '1': {
@@ -37,6 +77,121 @@ const BlogPosts = {
     description: 'Hello React Router v6'
   }
 };
+
+function Post() {
+  const { slug } = useParams();
+  const post = BlogPosts[slug];
+
+  const { title, description } = post;
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+      <Routes>
+        <Route index element={<Login auth={useAuth()}/>} />
+        {<Route path="login" element={<Login auth={useAuth()}/>} />}
+        <Route path='dashboard' element={<Dashboard />}>
+          <Route path="home" element={<Home />} />
+          <Route 
+            path="/about" 
+            element={ 
+              <ProtectedRoute>
+                <About />
+              </ProtectedRoute>} 
+            />
+          <Route path="posts" 
+            element={
+              <ProtectedRoute>
+                <Posts />
+              </ProtectedRoute>}
+              >
+            <Route path="/" element={<PostLists />} />
+            <Route path=":slug" element={<Post />} />
+          </Route>
+        </Route>
+      </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+const Navigation = () => {
+  const { token, onLogout } = useAuth();
+  return(
+    <nav style={{ margin: 10 }}>
+        <Link to="/home" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+        <Link to="/posts" style={{ padding: 5 }}>
+          Posts
+        </Link>
+      {token && (
+        <button type="button" onClick={onLogout}>
+          Sign Out
+        </button>
+      )}
+      {!token && <Navigate replace to="/login" />}
+      </nav>
+  )
+}
+const Home = () => {
+  const { onLogin } = useAuth();
+
+  return (
+    <>
+      <h2>Home (Public)</h2>
+
+      <button type="button" onClick={onLogin}>
+        Sign In
+      </button>
+    </>
+  );
+};
+
+const Login = props => {
+  const { onLogin } = useAuth();
+  return(
+      <div>
+          <h1>Login</h1>
+          <button type="button" onClick={onLogin}>
+              Sign In
+          </button>
+          <Outlet />
+      </div>
+  )
+}
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navigation />
+      <Outlet />
+    </div>
+  )
+}
+function About() {
+  const { token } = useAuth();
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      <div>Authenticated as {token}</div>
+    </div>
+  );
+}
 
 function Posts() {
   return (
@@ -61,46 +216,4 @@ function PostLists() {
     </ul>
   );
 }
-
-function Post() {
-  const { slug } = useParams();
-  const post = BlogPosts[slug];
-
-  const { title, description } = post;
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <nav style={{ margin: 10 }}>
-        <Link to="/" style={{ padding: 5 }}>
-          Home
-        </Link>
-        <Link to="/about" style={{ padding: 5 }}>
-          About
-        </Link>
-        <Link to="/posts" style={{ padding: 5 }}>
-          Posts
-        </Link>
-      </nav>
-      {/* Rest of the code remains same */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="posts" element={<Posts />}>
-          <Route path="/" element={<PostLists />} />
-          <Route path=":slug" element={<Post />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-}
-
 export default App;
